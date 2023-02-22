04517194a52e42cf7e71d13b775389db1a642bcd
Kyle Balston <kbalston@gmail.com>
2023-02-21 20:41:02 -0800

    Link to the day's puzzle in the `CalloutSolution` (#29)

--- docs/src/components/CalloutSolution.js
+++ docs/src/components/CalloutSolution.js
@@ -2,19 +2,14 @@ import React from "react";
 import Admonition from "@theme/Admonition";
 
 export default function CalloutSolution({ day }) {
+  const linkPuzzle = `https://adventofcode.com/2022/day/${day}`;
+  const linkGitHub = `https://github.com/kbalston/advent-of-code-2022/tree/main/day${day}`;
+
   return (
     <Admonition type="info" icon="✅" title="Full solution">
       <p>
-        The full solution for this day's puzzle can be found on{" "}
-        <a
-          href={
-            "https://github.com/kbalston/advent-of-code-2022/tree/main/day" +
-            day
-          }
-        >
-          GitHub
-        </a>
-        .
+        The full solution for <a href={linkPuzzle}>day {day}'s puzzle</a> can be
+        found on <a href={linkGitHub}>GitHub</a>.
       </p>
     </Admonition>
   );
